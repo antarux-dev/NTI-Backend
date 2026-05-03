a15a93b20e63ffc116b42b8297a06cab8edb8a1c
Antarux <theAntarux@proton.me>
2026-05-03 17:33:35 +0200

    eslint update & removed vue eslint prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import prettierConfig from '@vue/eslint-config-prettier';
+import prettierConfig from 'eslint-config-prettier';
 
 export default tseslint.config(
   { ignores: ['dist', 'node_modules', '*.config.js', '*.config.ts'] },
@@ -14,13 +14,9 @@ export default tseslint.config(
     languageOptions: {
       parser: tseslint.parser,
     },
-  },
-
-  {
     rules: {
       '@typescript-eslint/no-explicit-any': 'warn',
       '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-      'prettier/prettier': ['error', { endOfLine: 'auto' }],
       'no-console': ['warn', { allow: ['warn', 'error'] }],
       'no-debugger': 'error',
     },
