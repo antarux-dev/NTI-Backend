50180d1e3fe00e2befd922e9a9eeee6adcd3d1dc
Antarux <theAntarux@proton.me>
2026-05-13 22:41:27 +0200

    App health endpoint
    Novy app health endpoint pre rýchly "security check" stavu aplikácie. Je potrebné pridať payload pre check systémov, ale to je problém budúceho ja. Closes #13

--- src/features/health/controller.ts
+++ src/features/health/controller.ts
@@ -1,20 +1,12 @@
 import { Request, Response } from 'express';
 import { messageResponses } from './model.js';
 import { sendApiResponse } from '@utils/apiResponse.js';
-import { ValidationError } from '@utils/customErrors.js';
 
 export const getResponseMessage = (req: Request, res: Response) => {
-  if (Math.random() < 0.3) {
-    throw new ValidationError();
-  }
-
   const message = messageResponses[Math.floor(Math.random() * messageResponses.length)];
   sendApiResponse(res, {
     status: 200,
     success: true,
-    message: "Generated random message!",
-    data: {
-      message: message
-    },
+    message: message,
   });
 };
