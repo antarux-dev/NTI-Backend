478fffb66d81aeb4e505238e06b8079039bcb4ad
Antarux <theAntarux@proton.me>
2026-05-13 22:55:05 +0200

    Removed global route prefix

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,9 +3,8 @@ import healthRouter from '@features/health/route.js';
 import { NotFoundError } from '@utils/customErrors.js';
 
 const router = Router();
-const API_PREFIX = 'v1';
 
-router.use(`/${API_PREFIX}/health`, healthRouter);
+router.use(`/health`, healthRouter);
 
 router.use((req: Request, res: Response, next: NextFunction) => {
   next(new NotFoundError(`These aren't the endpoints you're looking for.`));
